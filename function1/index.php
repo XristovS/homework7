6d4ed121f506112d577561918068d6a9ae43e8a3
Stoyan Hristov <buffon_slaviq93@abv.bg>
2016-03-29 02:36:09 +0300

    new homework

---
+++ function1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+      function number ($numbes = 350 >= 274) {
+          $a = 274;
+          $b = 350;
+     echo "The number is: $numbes <br>" ;
+}
+
+number(350);
+        ?>
+    </body>
+</html>
---
+++ functions/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        function Ivan() {
+            $a = "Ivan";
+     echo "Hello-$a!";
+}
+Ivan();
+        ?>
+    </body>
+</html>
